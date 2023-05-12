79b007292dd1121a0a9fce96b6375c326fc987cc
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-05-12 15:59:08 +0200

    add LeftMenu

---
+++ components/LeftMenu/LeftMenu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './LeftMenu.css'
+
+function LeftMenu() {
+
+    return (
+        <div className="menu-wraper">
+
+        </div>
+    )
+}
+
+export default LeftMenu
