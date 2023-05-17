ab8289ddcb09e081496332fe0918b6d05fff11b3
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-05-17 18:41:03 +0200

    work on map <span

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,26 @@ import './App.css'
 
 
 function App() {
-  const [selector, setSelector] = useState('posts')
+  const [selector, setSelector] = useState('/laureates')
   const [data, setData] = useState([])
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${selector}`)
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  }, [])
-  useEffect(() => {
     const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-    fetch('https://api.nobelprize.org/2.1/laureates', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
-  })
-  console.log(selector)
+    fetch(`https://api.nobelprize.org/2.1${selector}`, options)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [selector])
+  console.log(data.laureates)
   return (
     <div className='App paddings'>
       <div className="window-container innerWidth padding flexStart ">
         <LeftMenu>
-          <button className="selector-button" onClick={() => setSelector('posts')}>Posts</button>
-          <button className="selector-button" onClick={() => setSelector('todos')}>Todos</button>
-          <button className="selector-button" onClick={() => setSelector('photos')}>Photos</button>
+          <button className="selector-button" onClick={() => setSelector('/laureates')}>Posts</button>
+          <button className="selector-button" onClick={() => setSelector('/nobelPrizes')}>Todos</button>
+          <button className="selector-button" onClick={() => setSelector('/nobelPrizes')}>Photos</button>
         </LeftMenu>
-        <RightSection data={data}>
+        <RightSection data={data.laureates}>
 
         </RightSection>
       </div>
