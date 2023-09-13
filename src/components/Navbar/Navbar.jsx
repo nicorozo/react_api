3d4bb419c4448f40b6091057989905fab29885a3
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-09-13 11:08:20 +0200

    phone version update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SiCircle } from "react-icons/si";
 import { BsGithub } from "react-icons/bs"
+import { FiMenu } from 'react-icons/fi'
+import { IoClose } from 'react-icons/io5'
 import './Navbar.css'
 
 
 
 const Navbar = () => {
+    const [isMenuBtn, setIsMenuBtn] = useState(true)
+    function mobileMenu() {
+        //const menuBtn = document.querySelector('.mobile_menu-btn')
+        const menuModal = document.querySelector('.leftMenu-wrapper')
+        if (isMenuBtn) {
+            menuModal.style.left = '0px'
+        } else if (!isMenuBtn) {
+            menuModal.style.left = '-100%'
+        }
+        setIsMenuBtn(!isMenuBtn)
+    }
+    const menuIcon = isMenuBtn ? <FiMenu className='mobile_menu-icon' /> : <IoClose className='mobile_menu-icon' />
 
     return (
         <div className="navbar">
@@ -19,8 +33,12 @@ const Navbar = () => {
                     <h2 className="innerWidth nav-logo">Nico Rozo </h2>
                 </div>
             </a>
+            <div className="mobile_menu">
+                <button className="mobile_menu-btn" onClick={mobileMenu}>{menuIcon}</button>
+            </div>
         </div>
     )
+
 }
 
 export default Navbar
