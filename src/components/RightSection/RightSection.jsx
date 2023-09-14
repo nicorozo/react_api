36099ad4bdaf0db086a76343e384282825088f00
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-09-14 17:02:02 +0200

    intro details

--- src/components/RightSection/RightSection.jsx
+++ src/components/RightSection/RightSection.jsx
@@ -8,14 +8,19 @@ const IntroText = () => {
     <div className="paddings innerWidth flextStart introduction">
       <h1 className='font-bold text-2xl mb-4'>Nobel Prize API</h1>
       <p>Welcome to the Nobel Prize API, a platform born out of a deep motivation and inspiration to share the remarkable stories of human achievement and progress. The Nobel Prize stands as a symbol of excellence, honoring individuals and organizations that have made extraordinary contributions to society. By creating this API, we aim to provide a seamless gateway for researchers, developers, and enthusiasts to access and explore the wealth of knowledge and inspiration encapsulated within the Nobel Prize.</p>
-      <ul className='list-disc paddings'>
-        <li>Physics</li>
-        <li>Chemistry</li>
-        <li>Medicine</li>
-        <li>Literature</li>
-        <li>Peace</li>
-        <li>Economic Sciences</li>
-      </ul>
+      <div className="intro_section">
+        <ul className='intro_list'>
+          <li>Physics</li>
+          <li>Chemistry</li>
+          <li>Medicine</li>
+          <li>Literature</li>
+          <li>Peace</li>
+          <li>Economic Sciences</li>
+        </ul>
+        <div className="intro_img-container">
+          <img src="intro.gif" alt="" className='intro_img' loading='lazy' />
+        </div>
+      </div>
       <p></p>
     </div>
   )
